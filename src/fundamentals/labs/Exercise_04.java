a51c1afc4ce67b246344774c4bea255cf45dd7f6
andreahowes <ashcreek.howes@gmailcom>
2018-08-22 21:41:30 -0400

    Exercise 1 commit through intelliJ

--- src/fundamentals/labs/Exercise_04.java
+++ src/fundamentals/labs/Exercise_04.java
@@ -11,8 +11,13 @@ package fundamentals.labs;
 public class Exercise_04 {
 
     public static void main(String[] args) {
-
-        // write code here
-
+        double radius = 3.14;
+        int height = 5;
+        //I'm using the formula for volume for area (it didn't specify volume or surface area)
+        double area = Math.PI * (radius * radius) * height;
+        //cylinders are three dimensional so I'm using the formula for surface area instead of perimeter
+        double perimeter = (2 * Math.PI * radius * height) + (2 * Math.PI * (radius * radius));
+        System.out.println("The area is: " + area);
+        System.out.println("The perimeter is " + perimeter);
     }
 }
